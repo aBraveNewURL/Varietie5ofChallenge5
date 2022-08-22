e207017331d6c18ad06292c2a951dab033f46371
Benjamin Sokolik <sokolik_b@hotmail.com>
2022-08-21 18:40:17 -0600

    taking a breather

--- assets/main.js
+++ assets/main.js
@@ -1,16 +1,60 @@
 const m = moment();
 console.log(m.toString());
 
-var theDate = moment().format('MM/DD/YYYY')
-
+//manages the time-block section
+var theDate = moment().format('LL');
 document.getElementById("theDate").innerHTML = theDate;
+
+//*manages local or session storage feature
+var storeIt = document.getElementById().innerHTML;  
+localStorage.setItem("storeIt",JSON.stringify(storeIt));
+
+var retrieveIt = localStorage.getItem("storeIt");
+console.log("retrieveIt: ", JSON.parse(retrieveIt));
+
+localStorage.setItem('btn*', 'desc*');
+jQuery('saveBtn').click(function() {
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     //$("#desc9").html("<textarea id='test' rows='4' cols='100' />"); IDEA: Can also set to input type="text"
     //$('#test').val('stuff here');
     //event.stopPropagation();
 
-$("#desc9").click(function() {
-    console.log("clicked");
+// $("#desc9").click(function() {
+//     console.log("clicked");
 
 
-})
+// })
