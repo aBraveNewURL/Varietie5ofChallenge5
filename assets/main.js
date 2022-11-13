520e668718bf4a319e24898edfaecb2564631461
Benjamin Sokolik <sokolik_b@hotmail.com>
2022-11-13 16:26:06 -0700

    reworking JS, cleaning up comments

--- assets/main.js
+++ assets/main.js
@@ -1,7 +1,8 @@
-var m = moment();
+const m = moment();
 let mHours = moment().hours();
-console.log(m.toString);
-console.log(mHours);
+let past;
+let present;
+let future;
 
 //manages the time-block section
 var theDate = moment().format('LL');
@@ -9,10 +10,7 @@ document.getElementById("theDate").innerHTML = theDate;
 
 //manages local or session storage feature
 
-var saveBtn = document.querySelector(".saveBtn");
-
-//let hourExample = 'hour-9';
-//parseInt(hourExample.split('-')[1]) =  9
+let saveBtn = $('.saveBtn');
 
 $('.saveBtn').on("click", function () {
     var currentData = $('body').html();
@@ -40,23 +38,7 @@ function renderStored() {
 };
 renderStored();
 
-// GIVEN I am using a daily planner to create a schedule
-// ?WHEN I open the planner
-// ?THEN the current day is displayed at the top of the calendar
-// ?WHEN I scroll down
-// ?THEN I am presented with timeblocks for standard business hours
-// !WHEN I view the timeblocks for that day
-// !THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-// ?WHEN I click into a timeblock
-// ?THEN I can enter an event
-// !WHEN I click the save button for that timeblock
-// !WHEN I refresh the page
-// !THEN the saved events persist
 
-
-// while i = 9; i < 18; i++; {
-
-// };
 console.log("The time is "+ mHours);
 
 function colorize() {
