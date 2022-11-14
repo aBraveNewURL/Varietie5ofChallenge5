6ca750277ea0717e9c90a25e5662f395d486edd1
Benjamin Sokolik <sokolik_b@hotmail.com>
2022-11-13 18:30:20 -0700

    updating for support

--- assets/main.js
+++ assets/main.js
@@ -5,34 +5,12 @@ let present;
 let future;
 let timeBlocks = $('.container');
 let timeBlockRow = $('.time-block');
-let saveBtn = $('.saveBtn');
 
 $('#currentDay').text(todayDate);
 
-// $('.saveBtn').on('click', () => {
-//     let currentData = $('body').html();
-//     let describeEl = $(this).siblings('.description').text();
-//     let hourEl = $(this).siblings('.hour').text();
-//     localStorage.setItem(hourEl, describeEl);
-//     localStorage.setItem('description', describeEl);
-//     localStorage.setItem('hour', hourEl);
-
-//     if (localStorage.getItem('.hour', describeEl) != currentData) {
-//         localStorage.getItem('.hour', describeEl);
-//         renderStored();
-//     }
-// });
-
-// function renderStored() {
-//     let gotHour = localStorage.getItem("hour");
-//     let gotDescription = localStorage.getItem("description");
-// };
-// renderStored();
-
-// console.log("The time is " + theDate);
-
-function setColor() {
+setColor = () => {
     let currentHour = moment().hour();
+    console.log("The hour is " + currentHour);
     timeBlockRow.each(function () {
         let blockHour = parseInt($(this).data('time'));
         if (currentHour === blockHour) {
@@ -45,5 +23,40 @@ function setColor() {
         }
     });
 };
-
 setColor();
+
+
+let saveButton = $('.saveBtn');
+saveButton.on('click', (event) => {
+    event.preventDefault()
+    let time1 = $(this).parent().parent().data('time')
+    let descriptionBox = $(this).siblings('.description').val();
+    localStorage.setItem('time', time1);
+    localStorage.setItem('description', descriptionBox)
+});
+
+
+renderStored = () => {
+
+    let descriptionInput = $('.description');
+    descriptionInput.each(function () {
+        console.log('descriptionInput= ' + descriptionInput)
+
+
+        let time2 = $(this).siblings('.hour')
+        console.log('time2= ' + time2)
+
+        let timeStored = localStorage.getItem('time');
+        console.log('timeStored= ' + timeStored)
+
+
+        let textStored = localStorage.getItem('description');
+        console.log('textStored= ' + textStored)
+        if (timeStored === time2) {
+            textStored.textContent.$(this)
+            console.log(textStored.textContent.$(this))
+
+        }
+    })
+};
+renderStored();
