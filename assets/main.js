4ab848aed13dada6cf92e8f1d0e12efed1ecf05f
Benjamin Sokolik <sokolik_b@hotmail.com>
2022-11-13 20:31:14 -0700

    updating for support3

--- assets/main.js
+++ assets/main.js
@@ -25,41 +25,34 @@ setColor = () => {
 };
 
 
-let saveButton = $('.saveBtn');
-$(saveButton).on('click', function() {
-    event.preventDefault();
-    // let time1 = $(this).parent().parent().data('time');
-    // let descriptionBox = $(this).siblings('.description').val();
-    let time = $(this).parent().data('time');
-    let description = $(this).siblings('.description').val();
-    localStorage.setItem('time', time);
-    localStorage.setItem('description', description);
-});
-
-
 renderStored = () => {
 
     let descriptionInput = $('.description');
     descriptionInput.each(function () {
-       
 
-
-        let time2 = $(this).siblings('.hour')
-      
+        let time2 = $(this).parent('.hour')
 
         let timeStored = localStorage.getItem('time');
-      
-
 
         let textStored = localStorage.getItem('description');
-      
+
         if (timeStored === time2) {
             textStored.textContent.$(this)
-            console.log(textStored.textContent.$(this))
-
-        } else {
-            console.log("Nothing yet")
         }
     })
 };
 renderStored();
+
+let saveButton = $('.saveBtn');
+$(saveButton).on('click', function () {
+    event.preventDefault();
+    // let time1 = $(this).parent().parent().data('time');
+    // let descriptionBox = $(this).siblings('.description').val();
+    let time1 = $(this).parent().parent().data('time');
+    let description = $(this).siblings('.description').val();
+    localStorage.setItem('time', time1);
+    localStorage.setItem('description', description);
+});
+
+
+
