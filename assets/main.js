ba2c4d59f8d9355df7f1a2c712f4f6d5db5f0d45
Benjamin Sokolik <sokolik_b@hotmail.com>
2022-11-23 19:17:17 -0700

    cleaned up, finishing touches

--- assets/main.js
+++ assets/main.js
@@ -7,17 +7,13 @@ let timeBlocks = $('.container');
 let timeBlockRow = $('.time-block');
 
 $('#currentDay').text(todayDate);
-// const image = data?.someval[i]?.image || "no image found"
-// const somethingelse = (data.someval.someother ? '2.00' : [])
+
 setColor = () => {
-    // TODO -- loop this for each block?
     let currentHour = moment().hour();
     console.log("The hour is " + currentHour);
     timeBlockRow.each(function () {
         let blockHour = parseInt($(this).data('time'));
-        
-        // (!(currentHour > blockHour)) ? $(this).addClass('past') : currentHour === blockHour ? $(this).addClass('present') : $(this).addClass('future')
-        
+
         if (currentHour > blockHour) {
             $(this).addClass('past')
         } else if (currentHour === blockHour) {
@@ -31,16 +27,15 @@ setColor = () => {
 setColor();
 
 renderStored = () => {
-    // TODO -- loop this for each hour?
     let descriptionInput = $('.hour');
     descriptionInput.each(function () {
-            
+
         let time2 = $(this).attr('id')
-            
+
         let timeStored = localStorage.getItem('time');
 
         let textStored = localStorage.getItem('description');
-            
+
         if (timeStored === time2) {
             $(this).siblings('.description').text(textStored);
         }
@@ -51,7 +46,6 @@ renderStored();
 let saveButton = $('.saveBtn');
 $(saveButton).on('click', function () {
     event.preventDefault();
-    // TODO -- loop this for each hour?
     let time1 = $(this).parent().parent().data('time');
     let description = $(this).siblings('.description').val();
     localStorage.setItem('time', time1);
